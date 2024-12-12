44cfb9f2b8396f1151260f41e28fd26ca9212ab4
Luuk <147055722+Lutrian1@users.noreply.github.com>
2024-12-12 22:29:06 +0100

    Test javascript

--- story.js
+++ story.js
@@ -1,7 +1,8 @@
 // Selecteer de play knop (zal een img zijn, dus dan class "playknop" in dit geval)
-let showMoreButton = document.querySelector('.show-more');
+let pauseButton = document.querySelector('.pausebutton');
 //Maak het clickable, dus dat je erop kan klikken
-
+pauseButton.addEventListener('click', function() {
 //Na het klikken veranderd die van play naar pauze
-
+    alert('Het werkt!');
+});
 //Omdat die nu op play staat, stopt het audio bestand.
