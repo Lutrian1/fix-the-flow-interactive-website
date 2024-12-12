2d2e1f55f048f955fa291be8c840c9ec21063fe1
Luuk <147055722+Lutrian1@users.noreply.github.com>
2024-12-12 13:27:36 +0100

    Fix #8
    Comments in JS

--- story.js
+++ story.js
@@ -1,1 +1,7 @@
+// Selecteer de play knop (zal een img zijn, dus dan class "playknop" in dit geval)
 
+//Maak het clickable, dus dat je erop kan klikken
+
+//Na het klikken veranderd die van play naar pauze
+
+//Omdat die nu op play staat, stopt het audio bestand.
