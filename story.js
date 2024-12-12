86a81e67ab96958ce2650ff4ab1983fe22aac906
Luuk <147055722+Lutrian1@users.noreply.github.com>
2024-12-12 22:26:14 +0100

    lol

--- story.js
+++ story.js
@@ -1,5 +1,5 @@
 // Selecteer de play knop (zal een img zijn, dus dan class "playknop" in dit geval)
-
+let showMoreButton = document.querySelector('.show-more');
 //Maak het clickable, dus dat je erop kan klikken
 
 //Na het klikken veranderd die van play naar pauze
